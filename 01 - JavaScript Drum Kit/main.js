ed82ff0de96494ab4eb8c25b26208c411f3eec89
Laura <moomal.abro@gmail.com>
2022-07-12 20:06:06 +0500

    changes to drum kit after watching video

--- 01 - JavaScript Drum Kit/main.js
+++ 01 - JavaScript Drum Kit/main.js
@@ -1,13 +1,15 @@
-let keys = document.querySelectorAll(".key");
-
-document.addEventListener('keydown', playDrum);
-document.addEventListener('keyup', stopDrum);
-
 function playDrum(e) {
    document.querySelector(`.key[data-key="${e.keyCode}"]`).classList.add("playing");
-   document.querySelector(`audio[data-key="${e.keyCode}"]`).play();
+   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+   // added this after watching the video
+   // learned you can manipulate the start poisition of an AV file
+   audio.currentTime = 0;
+   audio.play();
 }
 
 function stopDrum(e) {
    document.querySelector(`.key[data-key="${e.keyCode}"]`).classList.remove("playing");
 }
+
+document.addEventListener('keydown', playDrum);
+document.addEventListener('keyup', stopDrum);
