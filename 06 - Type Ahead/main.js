e8fbb86b879f860fb70bfc8996e9bb969417aad6
Laura <moomal.abro@gmail.com>
2022-07-17 18:49:08 +0500

    type ahead after watching the video

--- 06 - Type Ahead/main.js
+++ 06 - Type Ahead/main.js
@@ -1,34 +1,41 @@
+function fillData() {
+    const search = this.value;
+    const ul = document.querySelector('.suggestions');
+    if (search !== "") {
+        // cleaned this up
+        const regex = new RegExp(search, "gi");
+        let data = cities.filter(city => city.city.match(regex) || city.state.match(regex));
+        // changed from ul.appendChild to ul.innerHTML and the speed increased dramatically!
+        const html = data.map(city => {
+            // tried to use search before, thought it didn't work but it seems to be fine, must have changed something else along the way
+            let name = city.city.replace(regex, `<span class="hl">${search}</span>`);
+            let state = city.state.replace(regex, `<span class="hl">${search}</span>`);
+            return `<li><span class='left'>${name}, ${state}</span><span class='right'>${Number(city.population).toLocaleString()}</span></li>`;
+        }).join('');
+        ul.innerHTML = html;
+    } else {
+        ul.innerHTML = "<li>Filter for a city</li><li> or a state</li>";
+    }
+
+}
+
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+
+let cities = [];
 
 fetch(endpoint)
     .then((response) => {
         return response.json();
     })
     .then((data) => {
-        const input = document.querySelector('input');
-        input.addEventListener('keyup', (e) => fillData(data, input));
+        // cleaned this up
+        cities = data;
     })
     .catch((error) => {
         console.error('Error:', error);
     });
 
-function fillData(data, input) {
-    const search = input.value;
-    const ul = document.querySelector('.suggestions');
-    if (search !== "") {
-        data = data.filter(city => city.city.toLowerCase().includes(search.toLowerCase()) || city.state.toLowerCase().includes(search.toLowerCase()));
-        console.table(data);
-        ul.innerHTML = "";
-        for (city of data) {
-            let li = document.createElement('li');
-            let regex = new RegExp(`(${search})`, "gi");
-            let name = city.city.replace(regex, '<span class="hl">$1</span>');
-            let state = city.state.replace(regex, '<span class="hl">$1</span>');
-            li.innerHTML = `<span class='left'>${name}, ${state}</span><span class='right'>${Number(city.population).toLocaleString()}</span>`;
-            ul.appendChild(li);
-        }
-    } else {
-        ul.innerHTML = "<li>Filter for a city</li><li> or a state</li>";
-    }
+const input = document.querySelector('input');
 
-}
+// cleaned this up
+input.addEventListener('keyup', fillData);
