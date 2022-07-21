cd78037db876d8768b09418d344d59cf4a7d6915
Laura <moomal.abro@gmail.com>
2022-07-21 16:53:04 +0500

    shift checkboxes after video

--- 10 - Hold Shift and Check Checkboxes/main.js
+++ 10 - Hold Shift and Check Checkboxes/main.js
@@ -1,8 +1,10 @@
 function shiftSelect(e) {
-    if (!e.shiftKey) reuturn;
-    if (!lastCB) {
-        lastCB = this;
-    } else {
+    if (!lastCB) { lastCB = this }
+    // better logic for triggering the multi-check
+    else if (e.shiftKey && lastCB.checked === true) {
+        // wes's solution loops through the list and turns an 'inBetween' flag on when you hit lastCB or this and then turn it back off when you hit the other one
+        // then anything iterated while the flag is true gets checked
+        // it's cleaner than my solution but I'm happy with what I did
         let start = cbs.indexOf(lastCB);
         let end = cbs.indexOf(this);
         if (start > end) {
@@ -11,12 +13,12 @@ function shiftSelect(e) {
         let selectCBs = cbs.slice(start, end + 1);
         selectCBs.forEach(cb => cb.checked = true);
         lastCB = null;
-    };
-
+    }
 
 }
 
+
 const cbs = Array.from(document.querySelectorAll('input[type="checkbox"]'));
-let lastCB = null;
+let lastCB;
 
 cbs.forEach(cb => cb.addEventListener('click', shiftSelect));
