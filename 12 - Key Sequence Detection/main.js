e3368c3edf5c0bc8a44157dc78365c7d51ee90ac
Laura <moomal.abro@gmail.com>
2022-07-24 07:18:02 +0500

    konami code after watching video

--- 12 - Key Sequence Detection/main.js
+++ 12 - Key Sequence Detection/main.js
@@ -1,3 +1,6 @@
+// wes used a different method that creates an array from keypresses, joins it into a string, and compares it to a 'secret code' string.
+// I'm sticking with my method as it allows me to check for keys that would not be represented in strings
+
 function konami(e) {
     const konamiCode = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a', 'Enter'];
     if (e.key !== konamiCode[keyPos]) keyPos = 0;
